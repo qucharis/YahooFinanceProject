fa249c975232d7784a39c198615d24637b84b4e8
dszone <dschnzone@gmail.com>
2015-07-20 14:59:32 -0400

    src initial

---
+++ src/com/mercury/beans/Transaction.java
@@ -0,0 +1,11 @@
+package com.mercury.beans;
+
+public class Transaction {
+	private int transid;
+	private int userid;
+	private int sid;
+	private int amount;
+	private double unitprice;
+	private 
+
+}
