a8256b0763fe0efef0316358887f862c0f48c430
qucharis <quguodong2014@gmail.com>
2015-07-21 11:33:20 -0400

    modify StockDao.java

--- src/com/mercury/daos/StockDao.java
+++ src/com/mercury/daos/StockDao.java
@@ -1,12 +1,14 @@
 package com.mercury.daos;
 
+import java.util.*;
+
 import com.mercury.beans.Stock;
 import com.mercury.beans.User;
 
 public interface StockDao {
-	public void add(Stock stock);
-	public void delete(Stock stock);
-	public void buy(User user, Stock stock);
-	public void sell(User user, Stock stock);
-
+	public void addStock(Stock stock);
+	public void deleteStock(Stock stock);
+	public void buyStock(User user, Stock stock, int quantity);
+	public void sellStock(User user, Stock stock, int quantity);
+	public List<Stock> getStocksByUser(User user);	
 }
