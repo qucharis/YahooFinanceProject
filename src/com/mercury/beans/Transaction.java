aabbc43667552e5fdfd8e77d5dcceaf6904a4712
dszone <dschnzone@gmail.com>
2015-07-21 16:43:38 -0400

    hello controller and xml config

--- src/com/mercury/beans/Transaction.java
+++ src/com/mercury/beans/Transaction.java
@@ -5,8 +5,11 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -33,33 +36,54 @@ public class Transaction {
 	public void setTransid(int transid) {
 		this.transid = transid;
 	}
-	
-	
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="userid")
 	public User getUser() {
 		return user;
 	}
 	public void setUser(User user) {
 		this.user = user;
+		this.userid = user.getUserId();
 	}
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="sid")
 	public Stock getStock() {
 		return stock;
 	}
 	public void setStock(Stock stock) {
 		this.stock = stock;
+		this.sid = stock.getSid();
 	}
-	
+	@Column(name = "userid")
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	@Column(name="sid")
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	@Column(name ="amount")
 	public int getAmount() {
 		return amount;
 	}
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+	@Column(name = "uinitprice")
 	public BigDecimal getUnitprice() {
 		return unitprice;
 	}
 	public void setUnitprice(BigDecimal unitprice) {
 		this.unitprice = unitprice;
 	}
+	@Column(name="timestamp")
 	public Timestamp getTimestamp() {
 		return timestamp;
 	}
