46b3e1d44318de6624b05d7f3518f492f0fef165
dszone <dschnzone@gmail.com>
2015-07-22 16:53:22 -0400

    beans and dao,daoimple udate

--- src/com/mercury/controller/HelloController.java
+++ src/com/mercury/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.mercury.controller;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,30 +11,21 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mercury.beans.Stock;
+import com.mercury.beans.Transaction;
 import com.mercury.beans.User;
 import com.mercury.daos.StockDao;
 import com.mercury.daos.TransactionDao;
+import com.mercury.daos.UserDao;
 
 @Controller
 @SessionAttributes
 public class HelloController {
+	@Autowired
 	private TransactionDao td;
+	@Autowired
 	private StockDao sd;
-	public TransactionDao getTd() {
-		return td;
-	}
-
-	public void setTd(TransactionDao td) {
-		this.td = td;
-	}
-	
-	public StockDao getSd() {
-		return sd;
-	}
-
-	public void setSd(StockDao sd) {
-		this.sd = sd;
-	}
+	@Autowired
+	private UserDao ud;
 	
 /*	@RequestMapping(value="/hello", method=RequestMethod.POST)
 	public ModelAndView process(@ModelAttribute("user") 
@@ -43,6 +36,8 @@ public class HelloController {
 		mav.addObject("userInfo", userInfo);
 		return mav;
 	}*/
+
+	
 
 	@RequestMapping("/test")
 	public String test(){
@@ -55,14 +50,24 @@ public class HelloController {
 	@ResponseBody
 	public String mainPage() {	
 		
-		Stock stock = new Stock("google", "goo");
+		/*Stock stock = new Stock("yahoo", "yo");
 		sd.addStock(stock);
 		
 		User user = new User(10000,"qwer","123","abc@gmail.com",new BigDecimal(10),"admin",0);
-	
-		td.addUser(user);
+		ud.save(user);
+		Transaction tran = new Transaction(user,stock);
+		td.addTrans(tran);*/
 		
-		
+		Set<Transaction> set = td.queryTransAll();
+		System.out.println("*****************");
+		for(Transaction t:set){
+			System.out.println(t.getAmount()+";"+t.getTransid()+";"+t.getTimestamp()+";"+t.getUnitprice());
+		}
+		System.out.println("StockNames*******");
+		for(Transaction t:set){
+			System.out.println(t.getStock().toString());
+			System.out.println(t.getUser().getUserName() +":"+t.getUser().getPassword());
+		}
 		return "hello";
 	}
 	
