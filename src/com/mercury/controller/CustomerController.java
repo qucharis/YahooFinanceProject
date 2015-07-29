9916a326842e9b8d8ae82b2a3632a40a24003212
dszone <dschnzone@gmail.com>
2015-07-28 22:06:52 -0400

    pie chart added  and test method is in customer controller

--- src/com/mercury/controller/CustomerController.java
+++ src/com/mercury/controller/CustomerController.java
@@ -1,11 +1,25 @@
 package com.mercury.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.mercury.beans.StockInfo;
+import com.mercury.beans.Transaction;
+import com.mercury.beans.User;
 import com.mercury.daos.StockDao;
 import com.mercury.daos.TransactionDao;
 import com.mercury.daos.UserDao;
@@ -25,13 +39,29 @@ public class CustomerController {
 	private TransactionService tranSS;
 	@Autowired
 	private StockService ss;
-	
+
 	@RequestMapping("/addTran")
 	@ResponseBody
-	public String TransactionSubmmition(){
-		
-		
+	public String TransactionSubmmition() {
+
 		return null;
-		
+	}
+
+	@RequestMapping("/checkStock")
+	public ModelAndView checkStock(HttpServletRequest request) {
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("piechart");
+		User user = ud.getUserById(16);
+		Set<Transaction> trans = user.getTransactions();
+		List<StockInfo> infos = new ArrayList<StockInfo>();
+
+		for (Transaction t : trans) {
+			StockInfo si = ss.getInfo(t.getStock());
+			si.setAmount(t.getAmount());
+			infos.add(si);
+		}
+		mav.addObject("stockInfos", infos);
+		return mav;
 	}
 }
